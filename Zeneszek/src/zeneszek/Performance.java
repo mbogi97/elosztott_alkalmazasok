2d47a4bd7dd40ce79a54fb1d9bc0b5f79e5d0398
Müller Boglárka (D8IE65) <d8ie65@neptun.gdf.hu>
2022-05-22 18:36:34 +0200

    Add writing and reading from files

--- Zeneszek/src/zeneszek/Performance.java
+++ Zeneszek/src/zeneszek/Performance.java
@@ -1,10 +1,15 @@
 package zeneszek;
 
+import java.io.Serializable;
+
 /**
  *
  * @author D8IE65
  */
-public class Performance {
+public class Performance implements Serializable  {
+    
+    private static final long serialVersionUID = 1L;
+    
     private Musician musician;
     
     public Performance(Musician musician){
